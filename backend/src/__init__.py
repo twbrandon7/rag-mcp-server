07d659e75682bb48237207c5805d19fe766d55e0
CLX <twbrandon7@gmail.com>
2025-05-30 07:54:16 +0000

    feat(api): ✨ add authentication and user routes
    * Included `auth` and `users` routers in the main API router.
    * Set prefixes for the routes to organize the API structure.

--- backend/src/__init__.py
+++ backend/src/__init__.py
@@ -1,6 +1,10 @@
 from fastapi import APIRouter
 
 from src.utils import router as utils_router
+from src.auth.router import router as auth_router
+from src.users.router import router as users_router
 
 api_router = APIRouter()
 api_router.include_router(utils_router.router)
+api_router.include_router(auth_router, prefix="/auth", tags=["auth"])
+api_router.include_router(users_router, prefix="/users", tags=["users"])
