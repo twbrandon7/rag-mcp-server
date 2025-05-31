b04dbcccf8253656cb0059ce0023634f12b9f5c6
CLX <twbrandon7@gmail.com>
2025-05-31 07:34:55 +0000

    feat(api): ✨ add projects router to API
    * Included the `projects_router` in the main `api_router`.
    * This allows for handling project-related endpoints under the `/projects` path.

--- backend/src/__init__.py
+++ backend/src/__init__.py
@@ -3,8 +3,10 @@ from fastapi import APIRouter
 from src.utils import router as utils_router
 from src.auth.router import router as auth_router
 from src.users.router import router as users_router
+from src.projects.router import router as projects_router
 
 api_router = APIRouter()
 api_router.include_router(utils_router.router)
 api_router.include_router(auth_router, prefix="/auth", tags=["auth"])
 api_router.include_router(users_router, prefix="/users", tags=["users"])
+api_router.include_router(projects_router, prefix="/projects", tags=["projects"])
