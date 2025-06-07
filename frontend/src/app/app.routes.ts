af208d85b48533b787a1f1eb448d6c0a77f50318
CLX <twbrandon7@gmail.com>
2025-06-07 11:54:40 +0000

    feat(routes): ✨ add routing configuration for authentication and dashboard
    * Implemented routing for `auth` and `dashboard` paths.
    * Redirect root path to `dashboard`.

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { authRoutes } from './authentication/authentication.routes';
+import { dashboardRoutes } from './dashboard/dashboard.routes';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'auth',
+    children: authRoutes
+  },
+  {
+    path: 'dashboard',
+    children: dashboardRoutes
+  },
+  {
+    path: '',
+    redirectTo: 'dashboard',
+    pathMatch: 'full'
+  }
+];
