36bc33786fb30dad0795f9088e22a3739c0f92aa
CLX <twbrandon7@gmail.com>
2025-06-07 11:52:13 +0000

    feat(app): ✨ add HTTP client and PrimeNG configuration
    * Integrated `provideHttpClient` with `authInterceptor` for enhanced HTTP handling.
    * Configured `providePrimeNG` with the `Lara` theme for consistent styling.

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,8 +1,22 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { routes } from './app.routes';
+import { authInterceptor } from './core/auth.interceptor';
+import { providePrimeNG } from 'primeng/config';
+import Lara from '@primeng/themes/lara';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }), 
+    provideRouter(routes),
+    provideHttpClient(
+      withInterceptors([authInterceptor])
+    ),
+    providePrimeNG({
+      theme: {
+        preset: Lara
+      }
+    })
+  ]
 };
